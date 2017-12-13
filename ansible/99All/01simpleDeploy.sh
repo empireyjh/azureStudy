04af113edc4423cdabaa0091a9a7393fef56e9b0
Yoo <jinyoo@ebay.com>
2017-12-14 08:40:41 +0900

    t

--- ansible/99All/01simpleDeploy.sh
+++ ansible/99All/01simpleDeploy.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 cd ../02vmWithARM
-/bin/bash 00.azuredeploy.sh -g minschoTestRG01 -n vmWithAnsible -u minsoojo -r minschoVaultRG01 -v minschoVault -s minschoPub -c dummy-init.yaml
+/bin/bash 00.azuredeploy.sh -g jinyooTestRG01 -n vmWithAnsible -u jinyoo -r jinyooVaultRG01 -v jinyooVault -s jinyooPub -c dummy-init.yaml
 cd ../04CommonTasks
 /usr/local/bin/ansible-playbook common_tasks.yml
 
